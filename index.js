31bf4d3a3453fd7e590c052a2f57e9a436cbac57
Arnav2323 <91061223+Arnav2323@users.noreply.github.com>
2023-11-12 01:32:57 +0000

    live site

--- index.js
+++ index.js
@@ -1,30 +1,11 @@
 window.addEventListener("load", eventListeners);
-let windowHeight = window.innerHeight;
-let windowWidth = window.innerWidth;
 
-addEventListener("resize", (event) => {
-    windowHeight = window.innerHeight;
-    windowWidth = window.innerWidth;
-    console.log(`height: ${windowHeight} | width: ${windowWidth}`);
-    positionPages();
-});
-
-let pages = {
-    home: null,
-    about: null,
-    projects: null
+const buttons = {
 }
 
 function eventListeners(){
-    pages.home = document.querySelector('#home');
-    pages.about = document.querySelector('#about');
-    pages.projects = document.querySelector('#projects');
-    positionPages();
 }
 
-function positionPages(){
-    pages.about.style.top = windowHeight * 0.1 + "px";
-    console.log(pages.about.style.top);
-    pages.projects.style.top = windowHeight * 0.2 + "px";
-    console.log(pages.projects.style.top);
+function themeToggle(){
 }
+
